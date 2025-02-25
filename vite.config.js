5d63052352d09a03e69900cd0fa4348d6d800709
ali abdalla <info@appdepl.nl>
2025-02-25 11:43:26 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-
+# this is changes
 export default defineConfig({
     plugins: [
         laravel({
